4efadc8ba521e47b9589327ed655dc887b29d25b
cwd-harshit911 <sharmaharshit1405@gmail.com>
2023-05-06 12:58:32 +0530

    gg

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
+const cors = require("cors");
 
 const errorMiddleware = require("./middleware/error");
 
@@ -13,6 +14,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
