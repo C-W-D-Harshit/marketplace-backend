42b174d9adaddc5210ff3507a6b39f41897a0ae1
cwd-harshit911 <sharmaharshit1405@gmail.com>
2023-05-09 11:18:46 +0530

    .

--- app.js
+++ app.js
@@ -12,15 +12,16 @@ const errorMiddleware = require("./middleware/error");
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "config/config.env" });
 }
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: true,
-//     credentials: true,
-//   })
-// );
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
